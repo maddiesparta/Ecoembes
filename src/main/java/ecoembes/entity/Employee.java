4eded1eafd7349b442ecddc7363fb429988dd20c
Naia Martin <naia.martin@opendeusto.es>
2025-11-08 18:17:19 +0100

    Create Employee class

--- src/main/java/ecoembes/entity/Employee.java
+++ src/main/java/ecoembes/entity/Employee.java
@@ -1,5 +1,72 @@
 package ecoembes.entity;
 
-public class Employee {
+import java.util.Objects;
 
+public class Employee {
+	private int employee_id;
+	private String employee_name;
+	private String email;
+	private String password;
+	
+	public Employee() {
+	}
+	
+	public Employee(int employee_id, String employee_name, String email, String password) {
+		super();
+		this.employee_id = employee_id;
+		this.employee_name = employee_name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getEmployee_name() {
+		return employee_name;
+	}
+
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, employee_id, employee_name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(email, other.email) && employee_id == other.employee_id
+				&& Objects.equals(employee_name, other.employee_name) && Objects.equals(password, other.password);
+	}
+	
 }

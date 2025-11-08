0f60e26246724b09b5b6753d6c883c73b6f3c546
Maddi Esparta <maddi.esparta@opendeusto.es>
2025-11-08 17:14:15 +0100

    Initial commit: Spring Boot Ecoembes project

---
+++ src/main/java/com/example/demo/EcoembesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoembesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoembesApplication.class, args);
+	}
+
+}

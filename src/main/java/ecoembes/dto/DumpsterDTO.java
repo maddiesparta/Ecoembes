223dab8d9665514f8affa8032e84d5ff607fc8d9
Maddi Esparta <maddi.esparta@opendeusto.es>
2025-11-08 18:30:02 +0100

    Create Dumpster DTO

--- src/main/java/ecoembes/dto/DumpsterDTO.java
+++ src/main/java/ecoembes/dto/DumpsterDTO.java
@@ -1,5 +1,72 @@
 package ecoembes.dto;
 
 public class DumpsterDTO {
+	private String dumpster_id;
+	private String location;
+	private int postal_code;
+	private int capacity;
+	private float fill_level; //in Tons
+	private int container_number;
+	
+	public DumpsterDTO() {
+	}
+	
+	public DumpsterDTO(String dumpster_id, String location, int postal_code, int capacity, float fill_level,
+			int container_number) {
+		
+		this.dumpster_id = dumpster_id;
+		this.location = location;
+		this.postal_code = postal_code;
+		this.capacity = capacity;
+		this.fill_level = fill_level;
+		this.container_number = container_number;
+	}
+	
+	public String getDumpster_id() {
+		return dumpster_id;
+	}
+	
+	public void setDumpster_id(String dumpster_id) {
+		this.dumpster_id = dumpster_id;
+	}
 
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getPostal_code() {
+		return postal_code;
+	}
+
+	public void setPostal_code(int postal_code) {
+		this.postal_code = postal_code;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public float getFill_level() {
+		return fill_level;
+	}
+
+	public void setFill_level(float fill_level) {
+		this.fill_level = fill_level;
+	}
+
+	public int getContainer_number() {
+		return container_number;
+	}
+
+	public void setContainer_number(int container_number) {
+		this.container_number = container_number;
+	}
 }
--- src/main/java/ecoembes/entity/Allocation.java
+++ src/main/java/ecoembes/entity/Allocation.java
@@ -1,5 +1,75 @@
 package ecoembes.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Allocation {
+	private Long allocation_id;
+	private Long plant_id;
+	private Long employee_id;
+	private int dumpster_id;
+
+	public Allocation() {
+	}
+
+	public Allocation(Long allocation_id, Long plant_id, Long employee_id, int dumpster_id, Date date) {
+		super();
+		this.allocation_id = allocation_id;
+		this.plant_id = plant_id;
+		this.employee_id = employee_id;
+		this.dumpster_id = dumpster_id;
+	}
+
+	public Long getAllocation_id() {
+		return allocation_id;
+	}
+
+	public void setAllocation_id(Long allocation_id) {
+		this.allocation_id = allocation_id;
+	}
+
+	public Long getPlant_id() {
+		return plant_id;
+	}
+
+	public void setPlant_id(Long plant_id) {
+		this.plant_id = plant_id;
+	}
+
+	public Long getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(Long employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public int getDumpster_id() {
+		return dumpster_id;
+	}
+
+	public void setDumpster_id(int dumpster_id) {
+		this.dumpster_id = dumpster_id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(allocation_id, dumpster_id, employee_id, plant_id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Allocation other = (Allocation) obj;
+		return allocation_id == other.allocation_id;
+	}
+
+	
+	
 	
 }
